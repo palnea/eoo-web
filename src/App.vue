c8d341124f441f9f4e0cf90bf70f52f960e39e52
Onur Sezen <onur@palnea.com>
2024-02-29 21:19:18 +0300

    Move storage usage to Admin.vue

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,9 @@
 </template>
 
 <script setup>
-import { consoleWarning } from "@/utils/logger";
-import { onMounted } from "vue";
-import { useAuthStore } from "@/store/authStore";
 import SnackBar from "@/components/common/SnackBar.vue";
 import AppFooter from "@/components/app/AppFooter.vue";
 
-const authStore = useAuthStore();
-
-const fetchInitialData = async () => {
-  try {
-    await authStore.fetchUser()
-  } catch (err) {
-    consoleWarning("Failed to fetch user.")
-  } finally {
-    // loading.value = false
-  }
-}
-onMounted(fetchInitialData)
 </script>
 
 <style>
--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -1,5 +1,21 @@
 <script setup>
 import Drawer from "@/layouts/Drawer.vue";
+import { useAuthStore } from "@/store/authStore";
+import { consoleWarning } from "@/utils/logger";
+import { onMounted } from "vue";
+
+const authStore = useAuthStore();
+
+const fetchInitialData = async () => {
+  try {
+    await authStore.fetchUser()
+  } catch (err) {
+    consoleWarning("Failed to fetch user.")
+  } finally {
+    // loading.value = false
+  }
+}
+onMounted(fetchInitialData)
 </script>
 
 <template>
