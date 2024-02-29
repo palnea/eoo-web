7e4d6a90061a03ab41331e86c982ed8c205f2ef2
Onur Sezen <onur@palnea.com>
2024-02-29 12:31:47 +0300

    Disable store temporarily for Safari issue

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@
 <script setup>
 import { consoleWarning } from "@/utils/logger";
 import { onMounted } from "vue";
-import { useAuthStore } from "@/store/authStore";
+// import { useAuthStore } from "@/store/authStore";
 import SnackBar from "@/components/common/SnackBar.vue";
 import AppFooter from "@/components/app/AppFooter.vue";
 
-const authStore = useAuthStore();
+// const authStore = useAuthStore();
 
 const fetchInitialData = async () => {
   try {
